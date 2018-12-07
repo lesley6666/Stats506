4f3c60b2775625d12f44845249aab854fbe02a50
sophieyl93 <45380981+sophieyl93@users.noreply.github.com>
2018-12-06 19:46:31 -0500

    Add files via upload

--- soup.R
+++ soup.R
@@ -1,13 +1,17 @@
 library(MASS)
 library(data.table)
 library(ordinal)
+library(Hmisc)
 
 data(soup)
 soup_test = data.table(soup)
-head(soup_test)
+soup_test[1:10,]
   
-lapply(soup_test[,c("SOUPTYPE", "COLD", "SURENESS")], table)
-ftable(xtabs(~ SOUPTYPE + COLD + SURENESS, data = soup_test))
+summary(soup_test$SURENESS)
+mean(as.numeric(soup_test$SURENESS))
+
+lapply(soup_test[,c("SOUPTYPE", "SOUPFREQ", "COLD", "SURENESS")], table)
+ftable(xtabs(~ SOUPTYPE + SOUPFREQ + COLD + SURENESS, data = soup_test))
 
 m = polr(SURENESS ~ SOUPTYPE + COLD, data = soup_test, Hess = TRUE)
 summary(m)
@@ -21,6 +25,20 @@ p = pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 
 exp(coef(m))
 exp(cbind(OR = coef(m), ci))
+
+sf = function(y) {
+  c('Y>=1' = qlogis(mean(y >= 1)),
+    'Y>=2' = qlogis(mean(y >= 2)),
+    'Y>=3' = qlogis(mean(y >= 3)),
+    'Y>=4' = qlogis(mean(y >= 4)),
+    'Y>=5' = qlogis(mean(y >= 5)),
+    'Y>=6' = qlogis(mean(y >= 6)))
+}
+
+s = with(soup_test, 
+          summary(as.numeric(SURENESS) ~ SOUPTYPE + COLD, fun = sf))
+s
+
 
 newdat = data.frame(SOUPTYPE = soup_test$SOUPTYPE,
                     COLD = soup_test$COLD)
